96f5f343bf1575dd24a40da77c92212a26da7602
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-02-20 12:33:09 -0700

    define variables

---
+++ static/js/logic.js
@@ -0,0 +1,17 @@
+// Visualizing-Data-with-Leaflet - logic.js
+
+// Earthquakes & Tectonic Plates GeoJSON URL Variables
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+// Define Variables for tile Layers
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+});
+
+var grayscaleMap = L.tileLayer("")
+
+var outdoorsMap = L.tileLayer("")
